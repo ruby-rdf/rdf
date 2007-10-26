fcef919538ed310853de8a959f002438064369ff
Arto Bendiken <arto@bendiken.net>
2007-10-26 00:01:49 +0000

    Refactored RDF::Writer and separated out the default NTriples output to where it belongs.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rdfrb/trunk@7 581c2db1-bd1c-4451-8d2a-08a47a0b6355

--- lib/rdf/writer.rb
+++ lib/rdf/writer.rb
@@ -1,14 +1,36 @@
 module RDF
   class Writer
 
+    @@writers = []
+    @@file_extensions = {}
+    @@content_types = {}
+
     def self.format(format)
+      require "rdf/writers/#{format}"
+
       case format.to_sym
         when :ntriples then RDF::Writers::NTriples
         when :turtle then RDF::Writers::Turtle
-        when :n3 then RDF::Writers::N3
-        when :rdfxml, :"rdfxml-abbrev" then RDF::Writers::XML
+        when :notation3 then RDF::Writers::Notation3
+        when :rdfxml then RDF::Writers::RDFXML
         when :trix then RDF::Writers::TriX
       end
+    end
+
+    def self.each(&block)
+      if block_given?
+        @@writers.each { |writer| yield writer }
+      else
+        @@writers
+      end
+    end
+
+    def self.content_types
+      # TODO
+    end
+
+    def self.file_extensions
+      # TODO
     end
 
     def initialize(stream = $stdout, &block)
@@ -18,58 +40,48 @@ module RDF
       block.call(self) if block_given?
     end
 
-    def node_id
-      "_:n#{@node_id += 1}"
-    end
-
-    def register!(resource)
-      return false if @nodes[resource] # already seen it
-      @nodes[resource] = resource.uri || node_id
-    end
-
     def <<(resource)
       register!(resource) && write_node(resource)
     end
 
-    def escaped(string)
-      string.gsub("\\", "\\\\").gsub("\t", "\\\t").
-        gsub("\n", "\\\n").gsub("\r", "\\\r").gsub("\"", "\\\"")
-    end
+    protected
 
-    def quoted(string)
-      "\"#{string}\""
-    end
+      def self.inherited(child) #:nodoc:
+        @@writers << child
+        super
+      end
 
-    def write_node(resource)
-      nodes = []
+      def self.content_type(type, options = {})
+        @@content_types[type] ||= []
+        @@content_types[type] << self
 
-      s = resource.anonymous? ? @nodes[resource] : "<#{resource.uri}>"
-      resource.data.each do |predicate, objects|
-        p = "<#{predicate}>"
-
-        [objects].flatten.each do |object|
-          case object # FIXME: should use duck typing
-            when RDF::Resource
-              nodes << object if register!(object)
-              o = object.anonymous? ? @nodes[object] : "<#{object.uri}>"
-            when RDF::Literal
-              o = quoted(escaped(object.value))
-              o << "@#{language}" if object.language
-              o << "^^<#{object.type.uri}>" if object.type
-            else
-              o = quoted(escaped(object.to_s))
-          end
-
-          write_triple s, p, o
+        if options[:extension]
+          extensions = [options[:extension]].flatten.map { |ext| ext.to_sym }
+          extensions.each { |ext| @@file_extensions[ext] = type }
         end
       end
 
-      nodes.each { |node| write_node node }
-    end
+      def puts(*args)
+        @stream.puts(*args)
+      end
 
-    def write_triple(s, p, o)
-      @stream.puts "%s %s %s ." % [s, p, o]
-    end
+      def node_id
+        "_:n#{@node_id += 1}"
+      end
+
+      def register!(resource)
+        return false if @nodes[resource] # already seen it
+        @nodes[resource] = resource.uri || node_id
+      end
+
+      def escaped(string)
+        string.gsub("\\", "\\\\").gsub("\t", "\\\t").
+          gsub("\n", "\\\n").gsub("\r", "\\\r").gsub("\"", "\\\"")
+      end
+
+      def quoted(string)
+        "\"#{string}\""
+      end
 
   end
 
--- lib/rdf/writers/ntriples.rb
+++ lib/rdf/writers/ntriples.rb
@@ -1,5 +1,36 @@
 module RDF::Writers
   class NTriples < RDF::Writer
-    # TODO
+
+    def write_node(resource)
+      nodes = []
+
+      s = resource.anonymous? ? @nodes[resource] : "<#{resource.uri}>"
+      resource.data.each do |predicate, objects|
+        p = "<#{predicate}>"
+
+        [objects].flatten.each do |object|
+          case object # FIXME: should use duck typing
+            when RDF::URIRef
+              nodes << object if register!(object)
+              o = object.anonymous? ? @nodes[object] : "<#{object.uri}>"
+            when RDF::Literal
+              o = quoted(escaped(object.value))
+              o << "@#{language}" if object.language
+              o << "^^<#{object.type.uri}>" if object.type
+            else
+              o = quoted(escaped(object.to_s))
+          end
+
+          write_triple s, p, o
+        end
+      end
+
+      nodes.each { |node| write_node node }
+    end
+
+    def write_triple(s, p, o)
+      puts "%s %s %s ." % [s, p, o]
+    end
+
   end
 end
