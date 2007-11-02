0da6dd56161ec8960cd4542275052ffc193477c2
Arto Bendiken <arto@bendiken.net>
2007-11-02 00:45:10 +0000

    Implemented unit tests for the N-Triples reader.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rdfrb/trunk@26 581c2db1-bd1c-4451-8d2a-08a47a0b6355

--- test/test_readers.rb
+++ test/test_readers.rb
@@ -3,7 +3,27 @@ require 'rdf/readers'
 
 class TestReaders < Test::Unit::TestCase
   include RDF
-  include RDF::Readers
 
-  def test_; end
+  TEST_FILE = 'test/data/test.nt'
+
+  def open_test_file(&block)
+    RDF::Reader.open(TEST_FILE, :format => :ntriples, &block)
+  end
+
+  def test_open_ntriples_file
+    assert_nothing_raised { open_test_file }
+  end
+
+  def test_load_ntriples
+    assert_nothing_raised do
+      open_test_file do |input|
+        input.each_triple do |subject, predicate, object|
+          assert_kind_of Node, subject
+          assert_kind_of URIRef, predicate
+          assert_not_nil object
+        end
+      end
+    end
+  end
+
 end
