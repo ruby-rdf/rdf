2e867157ef5313a28b521502813bb020fb682fcd
Arto Bendiken <arto@bendiken.net>
2007-11-02 07:20:54 +0000

    Implemented unit tests for the TriX reader.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rdfrb/trunk@31 581c2db1-bd1c-4451-8d2a-08a47a0b6355

--- test/test_readers.rb
+++ test/test_readers.rb
@@ -4,19 +4,44 @@ require 'rdf/readers'
 class TestReaders < Test::Unit::TestCase
   include RDF
 
-  TEST_FILE = 'test/data/test.nt'
+  FILE_NTRIPLES = 'test.nt'
+  FILE_TRIX     = 'trix/examples.xml'
 
-  def open_test_file(&block)
-    RDF::Reader.open(TEST_FILE, :format => :ntriples, &block)
+  # N-Triples
+
+  def open_ntriples_file(&block)
+    RDF::Reader.open("test/data/#{FILE_NTRIPLES}", :format => :ntriples, &block)
   end
 
-  def test_open_ntriples_file
-    assert_nothing_raised { open_test_file }
+  def test_ntriples_open_file
+    assert_nothing_raised { open_ntriples_file }
   end
 
-  def test_load_ntriples
+  def test_ntriples_load
     assert_nothing_raised do
-      open_test_file do |input|
+      open_ntriples_file do |input|
+        input.each_triple do |subject, predicate, object|
+          assert_kind_of Node, subject
+          assert_kind_of URIRef, predicate
+          assert_not_nil object
+        end
+      end
+    end
+  end
+
+  # TriX
+
+  def open_trix_file(&block)
+    RDF::Reader.open("test/data/#{FILE_TRIX}", :format => :trix, &block)
+  end
+
+  def test_trix_open_file
+    assert_nothing_raised { open_trix_file }
+  end
+
+  def test_trix_load
+    assert_nothing_raised do
+      open_trix_file do |input|
         input.each_triple do |subject, predicate, object|
           assert_kind_of Node, subject
           assert_kind_of URIRef, predicate
