d8d1d85e11b83cad96be564fbe3ee8f3706cdbd1
Arto Bendiken <arto@bendiken.net>
2007-11-07 18:48:11 +0000

    Implemented unit tests for statements.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rdfrb/trunk@38 581c2db1-bd1c-4451-8d2a-08a47a0b6355

--- test/test_statements.rb
+++ test/test_statements.rb
@@ -3,5 +3,39 @@ require 'rdf'
 class TestStatements < Test::Unit::TestCase
   include RDF
 
-  def test_; end
+  CONTEXT   = URIRef.new("file://#{File.expand_path(__FILE__)}")
+  SUBJECT   = URIRef.new("")
+  PREDICATE = RDF::Namespaces::DOAP.name
+  OBJECT    = ""
+  TRIPLE    = [SUBJECT, PREDICATE, OBJECT]
+  QUAD      = TRIPLE + [{ :context => CONTEXT }]
+  HASH      = { SUBJECT => { PREDICATE => OBJECT } }
+
+  def test_statement_equality
+    assert Statement.new(*TRIPLE) == Statement.new(*TRIPLE)
+  end
+
+  def test_contextual_statements
+    assert !Statement.new(*TRIPLE).context?
+    assert Statement.new(*QUAD).context?
+    assert_equal CONTEXT, Statement.new(*QUAD).context
+  end
+
+  def test_asserted_statements
+    assert Statement.new(*TRIPLE).asserted?
+  end
+
+  def test_quoted_statements
+    assert !Statement.new(*TRIPLE).quoted?
+  end
+
+  def test_array_equivalence
+    assert_equal TRIPLE, Statement.new(*TRIPLE).to_a
+    assert Statement.new(*TRIPLE) == TRIPLE
+  end
+
+  def test_hash_equivalence
+    assert_equal HASH, Statement.new(*TRIPLE).to_hash
+  end
+
 end
