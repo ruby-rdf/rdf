af2a6a39dc44ee565a1d3eb922862934e15aaac3
Arto Bendiken <arto@bendiken.net>
2009-12-20 17:31:08 +0100

    Bumped version to 0.0.1.

--- lib/rdf/version.rb
+++ lib/rdf/version.rb
@@ -3,7 +3,12 @@ module RDF
     MAJOR = 0
     MINOR = 0
     TINY  = 1
+    EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING << "-#{EXTRA}" if EXTRA
+
+    def self.to_s()   STRING end
+    def self.to_str() STRING end
   end
 end
