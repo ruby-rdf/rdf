c7cf24d542d52a696f8b6436e1c6d03ec06969f5
Arto Bendiken <arto@bendiken.net>
2010-01-03 00:22:41 +0100

    Implemented RDF::Enumerable#each_statement and #enum_statement.

--- lib/rdf/enumerable.rb
+++ lib/rdf/enumerable.rb
@@ -3,13 +3,52 @@ module RDF
   # An RDF statement enumeration interface.
   #
   # Classes that include this module must implement an {#each} method that
-  # returns {RDF::Statement statements}.
+  # yields {RDF::Statement statements}.
   #
   # @see RDF::Graph
   # @see RDF::Repository
   module Enumerable
     extend ::Enumerable
 
-    # TODO
+    ##
+    # Iterates the given block for each RDF statement.
+    #
+    # If no block was given, returns an enumerator.
+    #
+    # @overload each_statement
+    #   @yield  [statement]
+    #   @yieldparam [Statement] statement
+    #   @return [void]
+    #
+    # @overload each_statement
+    #   @return [Enumerator]
+    #
+    # @return [void]
+    # @see #enum_statement
+    def each_statement(&block)
+      if block_given?
+        # Invoke {#each} in the containing class:
+        each(&block)
+      else
+        enum_statement
+      end
+    end
+
+    ##
+    # Returns an enumerator for {#each_statement}.
+    #
+    # @return [Enumerator]
+    # @see #each_statement
+    def enum_statement
+      require_enumerator!
+      ::Enumerable::Enumerator.new(self, :each_statement)
+    end
+
+    private
+
+      def require_enumerator! # @private
+        require 'enumerator' unless defined?(::Enumerable::Enumerator)
+      end
+
   end
 end
