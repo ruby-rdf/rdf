b5e1623441a673e690604696ba9bf9a0ebf52bfb
Arto Bendiken <arto@bendiken.net>
2010-01-04 04:19:41 +0100

    Imported Simple Knowledge Organization System (SKOS) properties.

--- lib/rdf/vocab/skos.rb
+++ lib/rdf/vocab/skos.rb
@@ -4,6 +4,33 @@ module RDF
   #
   # @see http://www.w3.org/TR/skos-reference/skos.html
   class SKOS < Vocabulary("http://www.w3.org/2004/02/skos/core#")
-    # TODO
+    property :altLabel
+    property :broadMatch
+    property :broader
+    property :broaderTransitive
+    property :changeNote
+    property :closeMatch
+    property :definition
+    property :editorialNote
+    property :exactMatch
+    property :example
+    property :hasTopConcept
+    property :hiddenLabel
+    property :historyNote
+    property :inScheme
+    property :mappingRelation
+    property :member
+    property :memberList
+    property :narrowMatch
+    property :narrower
+    property :narrowerTransitive
+    property :notation
+    property :note
+    property :prefLabel
+    property :related
+    property :relatedMatch
+    property :scopeNote
+    property :semanticRelation
+    property :topConceptOf
   end
 end
