8545d2a05743d18220ad0cfb86bc9f2486f0319d
Arto Bendiken <arto@bendiken.net>
2010-01-04 03:32:51 +0100

    Imported Hypertext Transfer Protocol (HTTP) properties.

--- lib/rdf/vocab/http.rb
+++ lib/rdf/vocab/http.rb
@@ -4,6 +4,23 @@ module RDF
   #
   # @see http://www.w3.org/2006/http
   class HTTP < Vocabulary("http://www.w3.org/2006/http#")
-    # TODO
+    property :abs_path
+    property :absoluteURI
+    property :authority
+    property :body
+    property :connectionAuthority
+    property :elementName
+    property :elementValue
+    property :fieldName
+    property :fieldValue
+    property :header
+    property :param
+    property :paramName
+    property :paramValue
+    property :request
+    property :requestURI
+    property :response
+    property :responseCode
+    property :version
   end
 end
