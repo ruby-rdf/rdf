498aa8f9db793f74a77409bd0c090ff22c59f777
Arto Bendiken <arto@bendiken.net>
2010-01-04 03:04:48 +0100

    Imported RDF Schema (RDFS) properties.

--- lib/rdf/vocab/rdfs.rb
+++ lib/rdf/vocab/rdfs.rb
@@ -4,6 +4,14 @@ module RDF
   #
   # @see http://www.w3.org/TR/rdf-schema/
   class RDFS < Vocabulary("http://www.w3.org/2000/01/rdf-schema#")
-    # TODO
+    property :comment
+    property :domain
+    property :isDefinedBy
+    property :label
+    property :member
+    property :range
+    property :seeAlso
+    property :subClassOf
+    property :subPropertyOf
   end
 end
