5e4526d047c4df2d9874c4e7875cabf3ae0542f8
Arto Bendiken <arto@bendiken.net>
2010-01-04 03:09:02 +0100

    Imported RDF Site Summary (RSS) properties.

--- lib/rdf/vocab/rss.rb
+++ lib/rdf/vocab/rss.rb
@@ -4,6 +4,11 @@ module RDF
   #
   # @see http://web.resource.org/rss/1.0/
   class RSS < Vocabulary("http://purl.org/rss/1.0/")
-    # TODO
+    property :description
+    property :items
+    property :link
+    property :name
+    property :title
+    property :url
   end
 end
