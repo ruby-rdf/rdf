22d7cb1dfa289597113fc033ac24493975d4efb1
Arto Bendiken <arto@bendiken.net>
2010-01-04 03:25:22 +0100

    Imported Description of a Project (DOAP) properties.

--- lib/rdf/vocab/doap.rb
+++ lib/rdf/vocab/doap.rb
@@ -4,6 +4,42 @@ module RDF
   #
   # @see http://trac.usefulinc.com/doap
   class DOAP < Vocabulary("http://usefulinc.com/ns/doap#")
-    # TODO
+    property :'anon-root'
+    property :audience
+    property :blog
+    property :browse
+    property :'bug-database'
+    property :category
+    property :created
+    property :description
+    property :developer
+    property :documenter
+    property :'download-mirror'
+    property :'download-page'
+    property :'file-release'
+    property :helper
+    property :homepage
+    property :implements
+    property :language
+    property :license
+    property :location
+    property :'mailing-list'
+    property :maintainer
+    property :module
+    property :name
+    property :'old-homepage'
+    property :os
+    property :platform
+    property :'programming-language'
+    property :release
+    property :repository
+    property :revision
+    property :screenshots
+    property :'service-endpoint'
+    property :shortdesc
+    property :tester
+    property :translator
+    property :vendor
+    property :wiki
   end
 end
