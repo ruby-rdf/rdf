6dabd183e27183e12fd335928a075dc29fada33a
Arto Bendiken <arto@bendiken.net>
2010-01-10 12:21:48 +0100

    Implemented the RDF::Readable mixin.

---
+++ lib/rdf/mixin/readable.rb
@@ -0,0 +1,13 @@
+module RDF
+  ##
+  module Readable
+    ##
+    # Returns `true` if `self` is readable.
+    #
+    # @return [Boolean]
+    # @see    RDF::Writable#writable?
+    def readable?
+      true
+    end
+  end
+end
