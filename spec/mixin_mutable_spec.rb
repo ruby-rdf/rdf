614a5644de0b95ef85da65e6787a1462061398a1
Ben Lavender <blavender@gmail.com>
2010-03-29 19:10:44 +0200

    Moved mutable specs to rdf-spec

--- spec/mixin_mutable_spec.rb
+++ spec/mixin_mutable_spec.rb
@@ -1,5 +1,5 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
-require 'rdf/ntriples'
+require 'rdf/spec/mutable'
 
 describe RDF::Mutable do
   before :each do
@@ -10,34 +10,6 @@ describe RDF::Mutable do
     @context    = RDF::URI.new("http://example.org/context")
   end
 
-  it "should support #load" do
-    @repo.respond_to?(:load).should be_true
-  end
+  it_should_behave_like RDF_Mutable
 
-  context "#load" do
-    it "should require an argument" do
-      lambda { @repo.load }.should raise_error(ArgumentError)
-    end
-
-    it "should accept a string filename argument" do
-      lambda { @repo.load(@filename) }.should_not raise_error(ArgumentError)
-    end
-
-    it "should accept an optional hash argument" do
-      lambda { @repo.load(@filename,{}) }.should_not raise_error(ArgumentError)
-    end
-
-    it "should load statements" do
-      @repo.load @filename
-      @repo.size.should ==  File.readlines(@filename).size
-      @repo.should have_subject @subject
-    end
-
-    it "should load statements with a context override" do
-      @repo.load @filename, :context => @context
-      @repo.should have_context @context
-      @repo.query(:context => @context).size.should == @repo.size
-    end
-
-  end
 end
