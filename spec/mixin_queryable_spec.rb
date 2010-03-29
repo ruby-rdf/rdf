1850a0e5fb6be6385392ff2d5d1e9eafa149d3e2
Ben Lavender <blavender@gmail.com>
2010-03-29 20:34:42 +0200

    Update queryable spec to use @filename

--- spec/mixin_queryable_spec.rb
+++ spec/mixin_queryable_spec.rb
@@ -3,8 +3,8 @@ require 'rdf/spec/queryable'
 
 describe RDF::Queryable do
   before :each do
-    @file       = etc_file("doap.nt")
-    @statements = RDF::NTriples::Reader.new(File.open(@file)).to_a
+    @filename   = etc_file("doap.nt")
+    @statements = RDF::NTriples::Reader.new(File.open(@filename)).to_a
     @queryable  = @statements.extend(RDF::Queryable)
     @subject    = RDF::URI.new("http://rubygems.org/gems/rdf")
   end
