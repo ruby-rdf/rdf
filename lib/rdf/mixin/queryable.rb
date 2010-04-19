d3e78120da8754f2dcabb7a1e4a927f207f74ff4
Arto Bendiken <arto@bendiken.net>
2010-04-19 23:03:35 +0200

    Implemented RDF::Queryable#first_{subject,predicate,object}.

--- lib/rdf/mixin/queryable.rb
+++ lib/rdf/mixin/queryable.rb
@@ -11,7 +11,7 @@ module RDF
     include ::Enumerable
 
     ##
-    # Queries `self` for RDF statements matching the given pattern.
+    # Queries `self` for RDF statements matching the given `pattern`.
     #
     # @example
     #     queryable.query([nil, RDF::DOAP.developer, nil])
@@ -22,7 +22,7 @@ module RDF
     # @yieldparam [Statement]
     # @return [Array<Statement>]
     def query(pattern, &block)
-      raise TypeError.new("#{self} is not readable") if respond_to?(:readable) && !readable?
+      raise TypeError.new("#{self} is not readable") if respond_to?(:readable?) && !readable?
 
       case pattern
         when Query
@@ -40,6 +40,87 @@ module RDF
         else
           raise ArgumentError.new("expected RDF::Query, RDF::Pattern, Array or Hash, but got #{pattern.inspect}")
       end
+    end
+
+    ##
+    # Queries `self` for an RDF statement matching the given `pattern` and
+    # returns the statement's subject term.
+    #
+    # Returns `nil` if no statements match `pattern`.
+    #
+    # @overload first_subject
+    #   @return [RDF::Resource]
+    #
+    # @overload first_subject(pattern)
+    #   @param  [Query, Statement, Array(Value), Hash] pattern
+    #   @return [RDF::Resource]
+    #
+    # @return [RDF::Resource]
+    def first_subject(pattern = nil)
+      if pattern
+        query(pattern) do |statement|
+          return statement.subject
+        end
+      else
+        each do |statement|
+          return statement.subject
+        end
+      end
+      return nil
+    end
+
+    ##
+    # Queries `self` for an RDF statement matching the given `pattern` and
+    # returns the statement's predicate term.
+    #
+    # Returns `nil` if no statements match `pattern`.
+    #
+    # @overload first_predicate
+    #   @return [RDF::URI]
+    #
+    # @overload first_predicate(pattern)
+    #   @param  [Query, Statement, Array(Value), Hash] pattern
+    #   @return [RDF::URI]
+    #
+    # @return [RDF::URI]
+    def first_predicate(pattern = nil)
+      if pattern
+        query(pattern) do |statement|
+          return statement.predicate
+        end
+      else
+        each do |statement|
+          return statement.predicate
+        end
+      end
+      return nil
+    end
+
+    ##
+    # Queries `self` for an RDF statement matching the given `pattern` and
+    # returns the statement's object term.
+    #
+    # Returns `nil` if no statements match `pattern`.
+    #
+    # @overload first_object
+    #   @return [RDF::Value]
+    #
+    # @overload first_object(pattern)
+    #   @param  [Query, Statement, Array(Value), Hash] pattern
+    #   @return [RDF::Value]
+    #
+    # @return [RDF::Value]
+    def first_object(pattern = nil)
+      if pattern
+        query(pattern) do |statement|
+          return statement.object
+        end
+      else
+        each do |statement|
+          return statement.object
+        end
+      end
+      return nil
     end
   end
 end
