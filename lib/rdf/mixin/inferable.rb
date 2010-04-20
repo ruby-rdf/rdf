460680da86bf5a475413fea71a9d1534d5787e49
Pius Uzamere <pius@uyiosa.com>
2010-04-20 15:03:48 +0800

    first pass at inferable, needs testing

--- lib/rdf/mixin/inferable.rb
+++ lib/rdf/mixin/inferable.rb
@@ -1,6 +1,41 @@
+require 'rdfs'
+
 module RDF
-  ##
   module Inferable
-    # TODO
+    include ::Queryable
+    
+    def pairs_of_statements
+      Enumerator.new(self, :combination, 2)
+    end
+    
+    def draw_inferences(rule_set = :rdfs_entailment)
+      case rule_set
+      when :rdfs_entailment
+        @learned_statements = []
+        each_statement {|statement| @learned_statements += unitary_inferences_from(statement)}
+        pairs_of_statements.each do |pair|
+          @learned_statements += pairwise_inferences_from pair
+        end
+      else
+        #todo
+      end
+      return @learned_statements
+    end
+
+    private
+    
+    def unitary_inferences_from(statement)
+      rules = RDFS::Semantics.constants.select {|c| Object.module_eval("RDFS::Semantics::#{c}").is_a? Class}
+      rules = rules.collect {|c| Object.module_eval("RDFS::Semantics::#{c}")}
+      inferred_statements = []
+      rules.each { |rule| inferred_statements += rule[statement] }
+    end
+    
+    def pairwise_inferences_from(pair)
+      rules = RDFS::Semantics.constants.select {|c| Object.module_eval("RDFS::Semantics::#{c}").is_a? Class}
+      rules = rules.collect {|c| Object.module_eval("RDFS::Semantics::#{c}")}
+      inferred_statements = []
+      rules.each { |rule| inferred_statements += rule[pair*] }
+    end
   end
 end
