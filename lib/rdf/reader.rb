36b4aea8031eb28c606546046b25be5dc174c61a
Arto Bendiken <arto@bendiken.net>
2010-12-20 11:48:01 +0100

    Changed RDF::Writer#prefix and RDF::Reader#prefix to pass through any given prefix value without trying to convert it to a symbol or assuming that it is an RDF::URI (closes #62).

--- lib/rdf/reader.rb
+++ lib/rdf/reader.rb
@@ -222,7 +222,7 @@ module RDF
     # @return [RDF::URI]
     def prefix(name, uri = nil)
       name = name.to_s.empty? ? nil : (name.respond_to?(:to_sym) ? name.to_sym : name.to_s.to_sym)
-      uri.nil? ? prefixes[name] : prefixes[name] = (uri.respond_to?(:to_sym) ? uri.to_sym : uri.to_s.to_sym)
+      uri.nil? ? prefixes[name] : prefixes[name] = uri
     end
     alias_method :prefix!, :prefix
 
