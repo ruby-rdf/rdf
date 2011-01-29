8637ce80c44532462ecdd5b88befe1739c7b1077
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-01-29 15:23:42 -0500

    Add RSpec filter configurations to allow running just focused specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'rdf/spec/matchers'
 
 RSpec.configure do |config|
   config.include(RDF::Spec::Matchers)
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
   config.exclusion_filter = {:ruby => lambda { |version|
     RUBY_VERSION.to_s !~ /^#{version}/
   }}
