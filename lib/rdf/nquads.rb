94f05d0b414b1e1e3ed0e4868c9a92c34c7c7c7e
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-10 12:49:04 -0800

    NQuads changes to help specs. (cherry-picking from commit b33dbfe8007f70f3e88d791c944f7e023cbf6908)

--- lib/rdf/nquads.rb
+++ lib/rdf/nquads.rb
@@ -4,9 +4,24 @@ module RDF
   #
   # This has not yet been implemented as of RDF.rb 0.3.x.
   module NQuads
-    class Format < NTriples::Format
-      # TODO
-    end # Format
+    ##
+    # N-Quads format specification.
+    #
+    # @example Obtaining an NQuads format class
+    #   RDF::Format.for(:nquads)     #=> RDF::NQuads::Format
+    #   RDF::Format.for("etc/doap.nq")
+    #   RDF::Format.for(:file_name      => "etc/doap.nq")
+    #   RDF::Format.for(:file_extension => "nq")
+    #   RDF::Format.for(:content_type   => "text/x-nquads")
+    #
+    # @see http://sw.deri.org/2008/07/n-quads/#mediatype
+    class Format < RDF::Format
+      content_type     'text/x-nquads', :extension => :nq
+      content_encoding 'ascii'
+
+      reader { RDF::NQuads::Reader }
+      writer { RDF::NQuads::Writer }
+    end
 
     class Reader < NTriples::Reader
       # TODO
