fedf81522fe637b0db915973a196748146bb91c4
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-12 14:47:08 -0700

    Define Writer#base_uri

--- lib/rdf/writer.rb
+++ lib/rdf/writer.rb
@@ -216,6 +216,18 @@ module RDF
     attr_reader :options
 
     ##
+    # Returns the base URI used for this writer.
+    #
+    # @example
+    #   reader.prefixes[:dc]  #=> RDF::URI('http://purl.org/dc/terms/')
+    #
+    # @return [Hash{Symbol => RDF::URI}]
+    # @since  0.3.4
+    def base_uri
+      @options[:base_uri]
+    end
+
+    ##
     # Returns the URI prefixes currently defined for this writer.
     #
     # @example
