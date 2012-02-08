0f4d4f3f76fe1a030bf517126fa141f6fd0d23b6
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-02-07 19:04:45 -0800

    Added Transaction spec using RDF::Transaction instance.

--- spec/mixin_queryable_spec.rb
+++ spec/mixin_queryable_spec.rb
@@ -3,6 +3,10 @@ require 'rdf/spec/queryable'
 
 describe RDF::Queryable do
   before :each do
+    # The available reference implementations are `RDF::Repository` and
+    # `RDF::Graph`, but a subclass of Ruby Array implementing
+    # `query_pattern` should do as well
+    # FIXME
     @queryable = RDF::Repository.new
   end
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,5 +1,9 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'rdf/spec/transaction'
 
 describe RDF::Transaction do
-  # TODO
+  before(:each) { @transaction = RDF::Transaction.new }
+  
+  # @see lib/rdf/spec/transaction.rb in rdf-spec
+  it_should_behave_like RDF_Transaction
 end
