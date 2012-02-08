533230db090aff24d75a3d5d234726a0076ef336
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-02-07 19:07:07 -0800

    Version 0.3.5

--- lib/rdf/version.rb
+++ lib/rdf/version.rb
@@ -1,10 +1,7 @@
 module RDF
   module VERSION
-    MAJOR = 0
-    MINOR = 3
-    TINY  = 4
-    EXTRA = 2
-
+    VERSION_FILE = File.expand_path("../../../VERSION", __FILE__)
+    MAJOR, MINOR, TINY, EXTRA = File.read(VERSION_FILE).chop.split(".")
     STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
 
     ##
