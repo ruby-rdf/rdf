f4975c83df52aee00872a79822cb436612399bd0
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-04-27 18:02:45 -0700

    Use RDF::Spec.triples to get access to test data rather than reading it from a local file.

--- spec/mixin_countable_spec.rb
+++ spec/mixin_countable_spec.rb
@@ -5,8 +5,7 @@ describe RDF::Countable do
   before :each do
     # The available reference implementations are `RDF::Repository` and
     # `RDF::Graph`, but a plain Ruby array will do fine as well:
-    @statements = RDF::NTriples::Reader.new(File.open('etc/doap.nt')).to_a
-    @countable  = @statements.dup.extend(RDF::Countable)
+    @countable = RDF::Spec.triples.extend(RDF::Countable)
   end
 
   # @see lib/rdf/spec/countable.rb in rdf-spec
--- spec/mixin_enumerable_spec.rb
+++ spec/mixin_enumerable_spec.rb
@@ -5,8 +5,7 @@ describe RDF::Enumerable do
   before :each do
     # The available reference implementations are `RDF::Repository` and
     # `RDF::Graph`, but a plain Ruby array will do fine as well:
-    @statements = RDF::NTriples::Reader.new(File.open('etc/doap.nt')).to_a
-    @enumerable = @statements.dup.extend(RDF::Enumerable)
+    @enumerable = RDF::Spec.triples.extend(RDF::Enumerable)
   end
 
   # @see lib/rdf/spec/enumerable.rb in rdf-spec
