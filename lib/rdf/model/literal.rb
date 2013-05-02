a5dba5ef47aeabaf459bb8fbbb5999f2fc124aed
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-05-01 19:01:10 -0700

    Calculate hashs before freezing literals.

--- lib/rdf/model/literal.rb
+++ lib/rdf/model/literal.rb
@@ -118,9 +118,9 @@ module RDF
       literal
     end
 
-    TRUE  = RDF::Literal.new(true).freeze
-    FALSE = RDF::Literal.new(false).freeze
-    ZERO  = RDF::Literal.new(0).freeze
+    TRUE  = RDF::Literal.new(true)
+    FALSE = RDF::Literal.new(false)
+    ZERO  = RDF::Literal.new(0)
 
     # @return [Symbol] The language tag (optional).
     attr_accessor :language
@@ -202,6 +202,14 @@ module RDF
     # @return [Fixnum]
     def value_hash
       @value_hash ||= value.hash
+    end
+
+    ##
+    # @private
+    def freeze
+      hash.freeze
+      value_hash.freeze
+      super
     end
 
     ##
