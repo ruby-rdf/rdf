4525496ecbb550108e7dd7132fe860fbd50d1ff3
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-07-09 11:18:07 -0600

    Recover from #to_time raising an exception to parse the string value. This fixes issue #87.

--- lib/rdf/model/literal/time.rb
+++ lib/rdf/model/literal/time.rb
@@ -22,7 +22,7 @@ module RDF; class Literal
       @string   ||= value if value.is_a?(String)
       @object   = case
         when value.is_a?(::Time)         then value
-        when value.respond_to?(:to_time) then value.to_time
+        when value.respond_to?(:to_time) then value.to_time rescue ::Time.parse(value.to_s)
         else ::Time.parse(value.to_s)
       end rescue nil
     end
@@ -67,7 +67,6 @@ module RDF; class Literal
         @object.strftime('%H:%M:%S%:z').
         sub(/\+00:00|UTC|GMT/, 'Z')
       else
-        # FIXME: verify
         # JRuby doesn't do timezone's properly, use utc_offset
         off = @object.utc_offset == 0 ? "Z" : ("%0.2d:00" % (@object.utc_offset/3600))
         @object.strftime("%H:%M:%S#{off}")
