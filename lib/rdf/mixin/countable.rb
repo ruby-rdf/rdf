e04c011034cb498c47c8c897247f3a6277e70015
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-10-09 16:35:49 -0700

    Update Countable#enum_for to use Countable::Enumerator rather than ::Enumerator extended with RDF::Countable. This parially addresses performance issues noted in issue #123

--- lib/rdf/mixin/countable.rb
+++ lib/rdf/mixin/countable.rb
@@ -31,8 +31,15 @@ module RDF
     # @see    Object#enum_for
     def enum_for(method = :each, *args)
       # Ensure that enumerators support the `#empty?` and `#count` methods:
-      super.extend(RDF::Countable)
+      this = self
+      Countable::Enumerator.new do |yielder|
+        this.send(method, *args) {|y| yielder << y}
+      end
     end
     alias_method :to_enum, :enum_for
+
+    class Enumerator < ::Enumerator
+      include Countable
+    end
   end # Countable
 end # RDF
