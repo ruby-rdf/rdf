bdd36a0b83118c081e651047d5a80fe1e0e513b6
Gregg Kellogg <gregg@greggkellogg.net>
2015-06-13 12:16:12 -0700

    Improve Indexable and Readable specs. For #202.

--- spec/mixin_indexable.rb
+++ spec/mixin_indexable.rb
@@ -2,11 +2,14 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 require 'rdf/spec/indexable'
 
 describe RDF::Indexable do
-  before :each do
-    @indexable = double("Indexable")
-    @indexable.extend(RDF::Indexable)
+  # @see lib/rdf/spec/indexable.rb in rdf-spec
+  it_behaves_like 'an RDF::Indexable' do
+    # The available reference implementations are `RDF::Repository` and
+    # `RDF::Graph`, but a double will do fine as well:
+    let(:indexable) do
+      d = double("Indexable").extend(RDF::Indexable)
+      allow(d).to receive(:index!) {d}
+      d
+    end
   end
-
-  # @see lib/rdf/spec/enumerable.rb in rdf-spec
-  include RDF_Indexable
 end
