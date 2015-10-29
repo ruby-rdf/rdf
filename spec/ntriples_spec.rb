b41b73f626dca9cf059c9b4481ad3c6b540a7c68
Gregg Kellogg <gregg@greggkellogg.net>
2015-10-29 16:07:43 -0700

    For N-Triples/Quads tests, test validation with statements, not graphs, as an incomplete statement will never be added to the graph. For #226.

--- spec/ntriples_spec.rb
+++ spec/ntriples_spec.rb
@@ -250,8 +250,7 @@ describe RDF::NTriples::Writer do
   context "validataion" do
     shared_examples "validation" do |statement, valid|
       context "given #{statement}" do
-        let(:graph) {RDF::Graph.new << statement}
-        subject {RDF::NTriples::Writer.buffer(validate: true) {|w| w << graph}}
+        subject {RDF::NTriples::Writer.buffer(validate: true) {|w| w << statement}}
 
         if valid
           specify {expect {subject}.not_to raise_error}
@@ -281,8 +280,7 @@ describe RDF::NTriples::Writer do
   context "c14n" do
     shared_examples "c14n" do |statement, result|
       context "given #{statement}" do
-        let(:graph) {RDF::Graph.new << statement}
-        subject {RDF::NTriples::Writer.buffer(validate: false, canonicalize: true) {|w| w << graph}}
+        subject {RDF::NTriples::Writer.buffer(validate: false, canonicalize: true) {|w| w << statement}}
         if result
           specify {expect(subject).to eq "#{result}\n"}
         else
