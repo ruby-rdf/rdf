4fec1ad48ad31ff7d3dc3449dd47204d5f86187b
Justin Coyne <jcoyne@justincoyne.com>
2015-10-30 14:59:27 -0500

    Don't compare twice

--- lib/rdf/model/uri.rb
+++ lib/rdf/model/uri.rb
@@ -739,7 +739,7 @@ module RDF
         # If other is a Literal, reverse test to consolodate complex type checking logic
         other == self
       when String then to_s == other
-      when URI then hash == other.hash && to_s == other.to_s
+      when URI then hash == other.hash
       else other.respond_to?(:to_uri) && to_s == other.to_uri.to_s
       end
     end
