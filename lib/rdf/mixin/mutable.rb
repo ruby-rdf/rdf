ruby-rdf/rdf

commit 22d6fea51802b15b16d117db0c51837db5b86d8a

Finish release-2_0

lib/rdf/mixin/mutable.rb
@@ -34,13 +34,12 @@ module RDF
     # @param  [String, #to_s]          filename
     # @param  [Hash{Symbol => Object}] options
     #   Options from {RDF::Reader.open}
-    # @option options [RDF::Resource] :context
-    #   Set set context of each loaded statement. This option is deprecated in RDF.rb 2.0.
     # @option options [RDF::Resource] :graph_name
     #   Set set graph name of each loaded statement
     # @return [void]
     def load(filename, options = {})
       if options.has_key?(:context)
+        raise ArgumentError, "The :contexts option to Mutable#load is deprecated in RDF.rb 2.0, use :graph_name instead. Called from #{Gem.location_of_caller.join(':')}" if RDF::Version.to_s >= '2.0'
         warn "[DEPRECATION] the :contexts option to Mutable#load is deprecated in RDF.rb 2.0, use :graph_name instead. Called from #{Gem.location_of_caller.join(':')}"
         options[:graph_name] ||= options.delete(:context)
       end
@@ -80,9 +79,20 @@ module RDF
     ##
     # Inserts RDF statements into `self`.
     #
-    # @param  [Array<RDF::Statement>] statements
-    # @raise  [TypeError] if `self` is immutable
-    # @return [Mutable]
+    # @note using splat argument syntax with excessive arguments provided
+    # significantly affects performance. Use Enumerator form for large
+    # numbers of statements.
+    #
+    # @overload insert(*statements)
+    #   @param  [Array<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
+    #
+    # @overload insert(statements)
+    #   @param  [Enumerable<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
+    #
     # @see    RDF::Writable#insert
     def insert(*statements)
       raise TypeError.new("#{self} is immutable") if immutable?
@@ -97,11 +107,22 @@ module RDF
     # `#delete([subject, predicate, nil])` followed by
     # `#insert([subject, predicate, object])` unless `object` is `nil`.
     #
-    # @param  [Enumerable<RDF::Statement>] statements
-    # @raise  [TypeError] if `self` is immutable
-    # @return [Mutable]
+    # @note using splat argument syntax with excessive arguments provided
+    # significantly affects performance. Use Enumerator form for large
+    # numbers of statements.
+    #
+    # @overload update(*statements)
+    #   @param  [Array<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
+    #
+    # @overload update(statements)
+    #   @param  [Enumerable<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
     def update(*statements)
       raise TypeError.new("#{self} is immutable") if immutable?
+      statements = statements[0] if statements.length == 1 && statements[0].is_a?(Enummerable)
 
       statements.each do |statement|
         if (statement = Statement.from(statement))
@@ -119,9 +140,19 @@ module RDF
     # considered to be a pattern, and used to query
     # self to find matching statements to delete.
     #
-    # @param  [Enumerable<RDF::Statement>] statements
-    # @raise  [TypeError] if `self` is immutable
-    # @return [Mutable]
+    # @note using splat argument syntax with excessive arguments provided
+    # significantly affects performance. Use Enumerator form for large
+    # numbers of statements.
+    #
+    # @overload delete(*statements)
+    #   @param  [Array<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
+    #
+    # @overload delete(statements)
+    #   @param  [Enumerable<RDF::Statement>] statements
+    #   @raise  [TypeError] if `self` is immutable
+    #   @return [self]
     def delete(*statements)
       raise TypeError.new("#{self} is immutable") if immutable?
 

commit 6ac37e8a29d380ca177400b4601f09ac76b00f9d

Fix a typo (`Enummerable`) in `RDF::Mutable`
This was introduced in 22d6fea51, leading up to the 1.99 release.

This conditional is apparently untested; apparently we never try to
`#update` on a single statement throughout the suite. I'm working on a
test to catch this.

I believe we should merge this immediately, since it is a simple typo
fix. Any issues revealed in adding `#update` testing can be dealt with
as an issue unrelated to the typo.

lib/rdf/mixin/mutable.rb
@@ -122,7 +122,7 @@ module RDF
     #   @return [self]
     def update(*statements)
       raise TypeError.new("#{self} is immutable") if immutable?
-      statements = statements[0] if statements.length == 1 && statements[0].is_a?(Enummerable)
+      statements = statements[0] if statements.length == 1 && statements[0].is_a?(Enumerable)
 
       statements.each do |statement|
         if (statement = Statement.from(statement))