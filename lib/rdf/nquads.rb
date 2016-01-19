5f009553035a3a6231fd61a0c5c44fbd12e11209
Gregg Kellogg <gregg@greggkellogg.net>
2016-01-18 18:01:17 -0800

    Remove `NQuads#write_statement`, as `Write#write_statement` now calls `write_quad` if it is implemented.

--- lib/rdf/nquads.rb
+++ lib/rdf/nquads.rb
@@ -89,21 +89,6 @@ module RDF
 
     class Writer < NTriples::Writer
       ##
-      # @param  [RDF::Statement] statement
-      # @return [self]
-      def write_statement(statement)
-        if statement.incomplete?
-          log_error "Statement #{statement.inspect} is incomplete"
-        elsif validate? && statement.invalid?
-          log_error "Statement #{statement.inspect} is invalid"
-        else
-          write_quad(*statement.to_quad)
-        end
-        self
-      end
-      alias_method :insert_statement, :write_statement # support the RDF::Writable interface
-
-      ##
       # Outputs the N-Quads representation of a statement.
       #
       # @param  [RDF::Resource] subject
