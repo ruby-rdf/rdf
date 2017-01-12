ruby-rdf/rdf

commit 18e8d4a7b3e086b0b075e9bd0a0b1aa7fd2758ba

No need to check for type in RDF::URI()

lib/rdf.rb
@@ -89,17 +89,14 @@ module RDF
   end
 
   ##
-  # Alias for `RDF::URI.new`.
+  # Cast to a URI. If already a URI, return the passed argument.
   #
   # @param (see RDF::URI#initialize)
   # @return [RDF::URI]
   def self.URI(*args, &block)
     case uri = args.first
-      when RDF::URI then uri
-      else case
-        when uri.respond_to?(:to_uri) then uri.to_uri
-        else URI.new(*args, &block)
-      end
+      when uri.respond_to?(:to_uri) then uri.to_uri
+      else URI.new(*args, &block)
     end
   end
 

commit 02c07528cfd5a0cef44bdaedbc241b5ffc8e8b1c

Fix regression introduced in 18e8d4a7b3e086b0b075e9bd0a0b1aa7fd2758ba which did not do what it attempted to.

lib/rdf.rb
@@ -94,10 +94,7 @@ module RDF
   # @param (see RDF::URI#initialize)
   # @return [RDF::URI]
   def self.URI(*args, &block)
-    case uri = args.first
-      when uri.respond_to?(:to_uri) then uri.to_uri
-      else URI.new(*args, &block)
-    end
+    (uri = args.first).respond_to?(:to_uri) ? uri.to_uri : URI.new(*args, &block)
   end
 
   ##