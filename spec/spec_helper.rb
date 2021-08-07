4903e9bd5e8e26baa1c487fc65ca7c57f762a997
Gregg Kellogg <gregg@greggkellogg.net>
2021-08-07 13:05:50 -0700

    Coveralls update.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
 require "bundler/setup"
 begin
   require 'simplecov'
+  require 'simplecov-lcov'
   require 'coveralls'
+
+  SimpleCov::Formatter::LcovFormatter.config do |config|
+    #Coveralls is coverage by default/lcov. Send info results
+    config.report_with_single_file = true
+    config.single_report_path = 'coverage/lcov.info'
+  end
+
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
+    SimpleCov::Formatter::LcovFormatter
   ])
   SimpleCov.start do
     add_group "Mixins", 'lib/rdf/mixin'
